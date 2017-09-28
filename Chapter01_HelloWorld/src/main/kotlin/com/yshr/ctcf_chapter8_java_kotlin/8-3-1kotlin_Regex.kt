06ccc9019466e4edebc3b5d776e44684faf875e1
ysr <yanshirong01@chinatopcredit.com>
2017-09-28 16:48:02 +0800

    代码提交

--- Chapter01_HelloWorld/src/main/kotlin/com/yshr/ctcf_chapter8_java_kotlin/8-3-1kotlin_Regex.kt
+++ Chapter01_HelloWorld/src/main/kotlin/com/yshr/ctcf_chapter8_java_kotlin/8-3-1kotlin_Regex.kt
@@ -5,7 +5,7 @@ package com.yshr.ctcf_chapter8_java_kotlin
  * Created by yshr on 2017/9/28.
  */
 fun main(args: Array<String>) {
-    val source = "hello, telephone num:010-8098765"""
+    val source = "hello, telephone num:010-8098765"
     val pattern = ".*(\\d{3}-\\d{8}).*"
     Regex(pattern).findAll(source).toList().flatMap(MatchResult::groupValues).forEach({
         println(it)
